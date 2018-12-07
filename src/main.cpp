f522dc957910ad6ef0e31ec5cc9fe103f3a5b8e7
Elias <supgesu@gmail.com>
2018-12-07 22:22:43 +0100

    set version in release script & new icon

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 
     QCoreApplication::setOrganizationName("Bleuzen");
     QCoreApplication::setApplicationName("FFaudioConverter");
-    QCoreApplication::setApplicationVersion("0.10.1");
+    QCoreApplication::setApplicationVersion("0.10.2");
 
     qDebug() << "Starting" << QCoreApplication::applicationName() + " v" + QCoreApplication::applicationVersion();
 
