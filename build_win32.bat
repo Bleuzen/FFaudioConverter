cef367925a11189e9dfb2ae13a102a5df5576fb2
Elias <supgesu@gmail.com>
2018-12-13 20:34:39 +0100

    added build script for windows

---
+++ build_win32.bat
@@ -0,0 +1,20 @@
+SET PATH=%PATH%;C:\Qt\5.12.0\mingw73_64\bin;C:\Qt\Tools\mingw730_64\bin
+
+mkdir build
+cd build
+
+qmake.exe ..\FFaudioConverter.pro -spec win32-g++
+mingw32-make.exe -j4
+
+cd release
+
+for /f "delims=" %%F in ('dir /b /a-d ^| findstr /vile ".exe"') do del "%%F"
+
+windeployqt.exe --release --compiler-runtime .
+
+rmdir /s /q iconengines imageformats styles translations
+del D3Dcompiler_47.dll Qt5Svg.dll opengl32sw.dll libGLESV2.dll libEGL.dll
+
+echo .
+echo Build done. Now download FFmpeg and place it in the applications directory.
+echo https://ffmpeg.zeranoe.com/builds/
