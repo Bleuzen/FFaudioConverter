5363aa5aa7a4fb56aed5a7606a5e143ebef76045
Bleuzen <supgesu@gmail.com>
2018-12-26 13:46:39 +0100

    first attempt to add translation support

--- release.sh
+++ release.sh
@@ -1,13 +1,17 @@
-#!/bin/sh 
+#!/bin/sh
+# Generate translations
+lrelease FFaudioConverter.pro
+
 VERSION=$(awk '{if(/QCoreApplication::setApplicationVersion/) print $0}' src/main.cpp)
 VERSION=${VERSION/QCoreApplication::setApplicationVersion(\"}
 VERSION=${VERSION/\");}
 VERSION=$(echo $VERSION | tr -d '\n')
 
+# Set version in appdata.xml
 sed -i "/    <release date=/c\    <release date=\"$(date +%Y-%m-%d)\" version=\"$VERSION\"/>" com.github.Bleuzen.FFaudioConverter.appdata.xml
 
+# Package
 mkdir -p release
-
 tar -cJf release/FFaudioConverter-$VERSION.tar.xz \
     src/ \
     FFaudioConverter.pro \
---
+++ update-translations.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+lupdate FFaudioConverter.pro
