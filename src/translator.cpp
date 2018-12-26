ffae266aa318b1af6fb8d6ad43347d24f6d11379
Bleuzen <supgesu@gmail.com>
2018-12-26 14:40:03 +0100

    made translations working

--- src/translator.cpp
+++ src/translator.cpp
@@ -26,13 +26,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <QLibraryInfo>
 #include <QDebug>
 
+QTranslator Translator::qtTranslator;
+QTranslator Translator::appTranslator;
+
 void Translator::init() {
     // Load Qt translations
-    QTranslator qtTranslator;
     qtTranslator.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
     qApp->installTranslator(&qtTranslator);
     // Load app translations
-    QTranslator appTranslator;
     QString defaultLocale = QLocale::system().name(); // e.g. "de_DE"
     defaultLocale.truncate(defaultLocale.lastIndexOf("_")); // e.g. "de"
     bool translationAvailable = appTranslator.load(QString(":/translations/i18n_%1.qm").arg(defaultLocale));
