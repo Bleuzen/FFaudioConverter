515682f23a1fb09dcaa2e6b4ab4d17e74059fd2e
Elias <supgesu@gmail.com>
2018-12-27 00:21:34 +0100

    fixed No matching signal Warning

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 
     QCoreApplication::setOrganizationName("Bleuzen");
     QCoreApplication::setApplicationName("FFaudioConverter");
-    QCoreApplication::setApplicationVersion("0.16.0");
+    QCoreApplication::setApplicationVersion("0.16.1");
 
     qDebug() << "Starting" << QCoreApplication::applicationName() + " v" + QCoreApplication::applicationVersion();
 
