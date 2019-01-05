80e7435f1d6df438c8861fe5ddfe5636fe4b4b15
Elias <supgesu@gmail.com>
2019-01-05 23:19:04 +0100

    added bass boost filter preset

--- src/settingsdialog.cpp
+++ src/settingsdialog.cpp
@@ -53,6 +53,7 @@ SettingsDialog::SettingsDialog(QWidget *parent) :
     ui->comboBox_AudioFiltersPresets->addItem(tr("Increase volume"), "volume=10dB");
     ui->comboBox_AudioFiltersPresets->addItem(tr("Reduce volume"), "volume=-5dB");
     ui->comboBox_AudioFiltersPresets->addItem(tr("Fade-in and fade-out"), "afade=duration=5\nareverse\nafade=duration=5\nareverse");
+    ui->comboBox_AudioFiltersPresets->addItem(tr("Bass boost"), "volume=-10dB\nbass=gain=10");
     ui->comboBox_AudioFiltersPresets->setCurrentIndex(-1);
 
     // Set settings in GUI
