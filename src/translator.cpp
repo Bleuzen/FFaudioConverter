8cde5d60a385a92a1052a8fbd9cb01b234d88a40
Elias <supgesu@gmail.com>
2019-01-05 22:26:57 +0100

    improved translations loading

--- src/translator.cpp
+++ src/translator.cpp
@@ -32,19 +32,24 @@ QTranslator Translator::qtTranslator;
 QTranslator Translator::appTranslator;
 
 void Translator::init() {
-    QString defaultLocale = QLocale::system().name(); // e.g. "de_DE"
-    defaultLocale.truncate(defaultLocale.lastIndexOf("_")); // e.g. "de"
+    QString defaultLocale = QLocale::system().name();
     loadTranslations(defaultLocale);
 }
 
 void Translator::loadTranslations(QString language) {
+    qDebug() << "Loading translations for" << language;
     // Load Qt translation
     if(qtTranslator.load("qt_" + language, QLibraryInfo::location(QLibraryInfo::TranslationsPath))) {
         qApp->installTranslator(&qtTranslator);
+        qDebug() << "Qt translation loaded";
+    } else {
+        qDebug() << "Qt translation not found";
     }
     // Load app translation
     if(appTranslator.load(language, ":/i18n/")) {
         qApp->installTranslator(&appTranslator);
+        qDebug() << "App translation loaded";
+    } else {
+        qDebug() << "App translation not found";
     }
-    qDebug() << "Translations loaded";
 }
